beb728699127be16daabc55ab3b9095adc15a7e0
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-01 17:02:56 +0300

    product card

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="content">
+    <img :src="image" />
+    <h3>{{ title }}</h3>
+    <p>{{ description }}</p>
+    <h6>{{ price }}</h6>
+    <ul>
+      <li><i class="fa fa-star" aria-hidden="true"></i></li>
+      <li><i class="fa fa-star" aria-hidden="true"></i></li>
+      <li><i class="fa fa-star" aria-hidden="true"></i></li>
+      <li><i class="fa fa-star" aria-hidden="true"></i></li>
+      <li><i class="fa fa-star" aria-hidden="true"></i></li>
+    </ul>
+    <button class="buy-1">Buy Now</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "ProductCard",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
