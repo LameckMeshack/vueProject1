bb8f1880d056ea7d5a15e1ccbd83ad18c38f7b63
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-01 23:08:13 +0300

    sorting backend

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,37 @@
-﻿require('rootpath')();
-const express = require('express');
+require("dotenv").config();
+
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const jwt = require('_helpers/jwt');
-const errorHandler = require('_helpers/error-handler');
+const jwt = require("jsonwebtoken");
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
+app.use(express.json());
 
-// use JWT auth to secure the api
-app.use(jwt());
+const posts = [
+  {
+    username: "Lameck",
+    title: "Intern 1",
+  },
+  {
+    username: "Sonwar",
+    title: "Intern 2",
+  },
+];
 
-// api routes
-app.use('/users', require('./users/users.controller'));
-
-// global error handler
-app.use(errorHandler);
-
-// start server
-const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
-const server = app.listen(port, function () {
-    console.log('Server listening on port ' + port);
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(posts.filter((post) => post.username === req.user.name));
 });
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(err);
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+app.listen(3000);
