bc8522d43ac361011284f9706ba42fb04aa40036
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-01 15:02:39 +0300

    added a loader components

--- src/components/ProductContainer.vue
+++ src/components/ProductContainer.vue
@@ -1,15 +1,36 @@
 <template>
-  <div class="hello">
-    <h1></h1>
+  <div>
+    <p v-if="loading"><Loading /></p>
+    <ul v-else>
+      <li v-for="(value, key) in products" :key="key">
+        {{ key }} : {{ value }}
+      </li>
+    </ul>
+    <p v-if="error">{{ error }}</p>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import Loading from "./Loading.vue";
 export default {
   name: "ProductContainer",
   props: {
     msg: String,
+  },
+  data() {
+    return { loading: false, products: null, error: "" };
+  },
+  methods: {
+    // async fetchProduct() {
+    //   try {
+    //     const response = await axios.get("https://fakestoreapi.com/products");
+    //     const products = response.data;
+    //     console.log(products);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // },
   },
   created: function () {
     this.loading = true;
@@ -17,14 +38,15 @@ export default {
       .get("https://fakestoreapi.com/products")
       .then((res) => {
         this.loading = false;
-        this.post = res.data;
-        console.log(this.post);
+        this.products = res.data;
+        console.log(this.products);
       })
       .catch((err) => {
         this.loading = false;
         this.error = err;
       });
   },
+  components: { Loading },
 };
 </script>
 
