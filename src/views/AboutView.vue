1a0163971466a3e435f684159871f6d690cd5d06
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-01 16:40:57 +0300

    about page to view single product

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <SingleProduct />
   </div>
 </template>
+<script>
+import SingleProduct from "@/components/SingleProduct.vue";
+
+export default {
+  name: "HomeView",
+  components: { SingleProduct },
+};
+</script>
