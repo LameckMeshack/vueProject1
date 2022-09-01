2e4a94291884331a2140a3add475ce89bed3781b
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-01 14:03:12 +0300

    fetching data in product components using axios

---
+++ src/components/ProductContainer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1></h1>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "ProductContainer",
+  props: {
+    msg: String,
+  },
+  created: function () {
+    this.loading = true;
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => {
+        this.loading = false;
+        this.post = res.data;
+        console.log(this.post);
+      })
+      .catch((err) => {
+        this.loading = false;
+        this.error = err;
+      });
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
