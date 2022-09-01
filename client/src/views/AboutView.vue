4f183ac235460174e2d1ec3f26d1b75f26e2a79a
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-01 20:21:06 +0300

    trying accessing about page

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="about">
+    <router-link v-if="!isLoggedIn" to="/login"
+      ><button>Login</button></router-link
+    >
     <SingleProduct />
   </div>
 </template>
 <script>
 import SingleProduct from "@/components/SingleProduct.vue";
+import { mapGetters } from "vuex";
 
 export default {
   name: "HomeView",
   components: { SingleProduct },
+
+  computed: {
+    ...mapGetters(["isLoggedIn"]),
+  },
 };
 </script>
