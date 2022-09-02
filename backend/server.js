9f5262d638b6eec7c0c159ad84aae326ca9c736a
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-02 08:41:51 +0300

    tokens verification

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,19 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
+app.use(cors());
 
 app.use(express.json());
-
+let refreshTokens = [];
 const posts = [
   {
     username: "Lameck",
-    title: "Intern 1",
+    title: "Post 1",
   },
   {
-    username: "Sonwar",
-    title: "Intern 2",
+    username: "Jim",
+    title: "Post 2",
   },
 ];
 
@@ -25,7 +27,6 @@ function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (token == null) return res.sendStatus(401);
-
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     console.log(err);
     if (err) return res.sendStatus(403);
@@ -33,5 +34,30 @@ function authenticateToken(req, res, next) {
     next();
   });
 }
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "72000s",
+  });
+}
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = { name: username };
+
+  const accessToken = generateAccessToken(user);
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  refreshTokens.push(refreshToken);
+  // res.json({ accessToken: accessToken, refreshToken: refreshToken });
+  res.json({ accessToken });
+});
+//logout
+
+app.delete("/logout", (req, res) => {
+  refreshTokens = refreshTokens.filter((token) => token !== req.body.token);
+  res.sendStatus(204);
+});
 
 app.listen(3000);
